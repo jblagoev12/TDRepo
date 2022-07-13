e991376f1d2305d3e3f11348ad00b5101c2d5523
Muthaiah <yogananda.muthaiah@sap.com>
2022-07-13 20:33:52 +0200

    new changesetid

--- SQLs/CR1.sql
+++ SQLs/CR1.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
---changeset yogananda:9998
+--changeset yogananda:9995
 --preconditions onFail:CONTINUE
 --comment: Initial creation of table Yogananda
 --validCheckSum: ANY
-Create Table Yogananda (
+Create Table Yogananda1 (
    ID INTEGER,
    NAME VARCHAR(10),
    PRIMARY KEY (ID)
@@ -13,7 +13,7 @@ Create Table Yogananda (
 
 
 --liquibase formatted sql
---changeset yogananda:999
+--changeset yogananda:991
 --preconditions onFail:CONTINUE
 --comment: Initial creation of table Yogananda
 --validCheckSum: ANY
