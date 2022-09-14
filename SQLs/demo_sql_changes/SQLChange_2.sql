5ac28a14cdab262a71c190f84de53f56b3d15faf
jblagoev12 <87875031+jblagoev12@users.noreply.github.com>
2022-09-14 11:52:50 -0400

    Revert "Version2"

--- SQLs/demo_sql_changes/SQLChange_2.sql
+++ SQLs/demo_sql_changes/SQLChange_2.sql
@@ -1,22 +1,7 @@
 --liquibase formatted sql
---changeset JBLAGOEV:DEMO_SQL_5 runOnChange=true failOnError:true endDelimiter:/ rollbackEndDelimiter:/
---rollback CREATE OR REPLACE PROCEDURE PROC_DEMO_SQL
---rollback (
---rollback    IN X INT,
---rollback    OUT Y INT	
---rollback )
---rollback LANGUAGE SQLSCRIPT
---rollback SQL SECURITY DEFINER
---rollback AS
---rollback BEGIN
---rollback  declare z int;
---rollback  
---rollback  select count(*) into z from TABLE_DEMO_SQL;
---rollback  
---rollback  Y = :X + :Z;
---rollback END;
---rollback /
---comment: Version 2 of procedure PROC_DEMO_SQL
+--changeset JBLAGOEV:DEMO_SQL_3 failOnError:true endDelimiter:/
+--rollback DROP PROCEDURE PROC_DEMO_SQL; 
+--comment: Initial creation of procedure PROC_DEMO_SQL
 --validCheckSum: ANY
 CREATE OR REPLACE PROCEDURE PROC_DEMO_SQL
 (
@@ -31,6 +16,7 @@ BEGIN
   
   select count(*) into z from TABLE_DEMO_SQL;
   
-  Y = :X + :Z + 1;
+  Y = :X + :Z; 
 END;
 /
+
